1ca997b0071217e03f49965b7918910c790de0c4
felipeac01 <2019.felipealcantara@gmail.com>
2020-01-19 01:55:38 -0200

    Inserindo meu arquivo php pela segunda vez

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Teste</title>
+	<title>Teste2</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="estilo.css">
