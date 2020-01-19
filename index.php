9e171d5773af35eb0d9c6288983b73b3a5311d3b
felipeac01 <2019.felipealcantara@gmail.com>
2020-01-19 01:52:23 -0200

    Inserindo meu arquivo php pela primeira vez

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+	<?php 
+		echo 'Eita carai'; 
+	?>
+</body>
+</html>
+
